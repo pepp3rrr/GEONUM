d3662a6609eb63efdbc07ab68201dd01abf36ca1
GaspardCulis <gaspard@gasdev.fr>
2025-02-01 11:57:50 +0100

    feat(bezier): Add `bounding_box` method

--- src/bezier.rs
+++ src/bezier.rs
@@ -11,6 +11,31 @@ impl Bezier {
         Self { control }
     }
 
+    pub fn compute(&self, t: f32) -> Point {
+        Self::compute_woker(t, self.control.clone())
+    }
+
+    fn compute_woker(t: f32, mut control: Vec<Point>) -> Point {
+        let n = control.len();
+
+        if n == 1 {
+            return control.pop().expect("Should not be empty");
+        }
+
+        let mut new = Vec::<Point>::new();
+        for k in 1..n {
+            let a = control.get(k - 1).unwrap().clone();
+            let b = control.get(k).unwrap().clone();
+
+            // (1-t)a + tb <=> a -ta + tb <=> a + t(b - a)
+            // This form respects point and vec operation rules
+            let coord = a + (b - a) * t;
+            new.push(coord);
+        }
+
+        Self::compute_woker(t, new)
+    }
+
     pub fn from_csv<P: AsRef<Path>>(path: P) -> Self {
         let file_reader = File::open(path).expect("Failed to read from CSV");
         let mut rdr = csv::ReaderBuilder::new()
@@ -36,28 +61,32 @@ impl Bezier {
         Self { control }
     }
 
-    pub fn compute(&self, t: f32) -> Point {
-        Self::compute_woker(t, self.control.clone())
-    }
+    pub fn bounding_box(&self) -> (Point, Point) {
+        let right = self
+            .control
+            .iter()
+            .map(|p| p.x)
+            .max_by(|a, b| a.total_cmp(&b))
+            .unwrap();
+        let top = self
+            .control
+            .iter()
+            .map(|p| p.y)
+            .max_by(|a, b| a.total_cmp(&b))
+            .unwrap();
+        let left = self
+            .control
+            .iter()
+            .map(|p| p.x)
+            .max_by(|a, b| b.total_cmp(&a))
+            .unwrap();
+        let bottom = self
+            .control
+            .iter()
+            .map(|p| p.y)
+            .max_by(|a, b| b.total_cmp(&a))
+            .unwrap();
 
-    fn compute_woker(t: f32, mut control: Vec<Point>) -> Point {
-        let n = control.len();
-
-        if n == 1 {
-            return control.pop().expect("Should not be empty");
-        }
-
-        let mut new = Vec::<Point>::new();
-        for k in 1..n {
-            let a = control.get(k - 1).unwrap().clone();
-            let b = control.get(k).unwrap().clone();
-
-            // (1-t)a + tb <=> a -ta + tb <=> a + t(b - a)
-            // This form respects point and vec operation rules
-            let coord = a + (b - a) * t;
-            new.push(coord);
-        }
-
-        Self::compute_woker(t, new)
+        (Point::new(left, bottom), Point::new(right, top))
     }
 }
