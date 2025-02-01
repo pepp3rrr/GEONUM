90b5eae004cdc9ff71c9b9e0325bf5c5e66ad564
GaspardCulis <gaspard@gasdev.fr>
2025-02-01 11:58:21 +0100

    feat(main): Now building from new CSV files

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,8 @@ use bezier::Bezier;
 use types::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let control = vec![Point::new(0., 0.), Point::new(1., 2.), Point::new(2., 0.)];
-    let control_str = format!(
-        "Control: {:?}",
-        control.iter().map(|p| (p.x, p.y)).collect::<Vec<_>>()
-    );
-    let bezier = Bezier::new(control);
+    let bezier = Bezier::from_csv("data/simple.bcv");
+    let bb = bezier.bounding_box();
 
     let root = BitMapBackend::new("/tmp/0.png", (640, 480)).into_drawing_area();
     root.fill(&WHITE)?;
@@ -22,20 +18,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .margin(5)
         .x_label_area_size(30)
         .y_label_area_size(30)
-        .build_cartesian_2d(0f32..2f32, 0f32..1f32)?;
+        .build_cartesian_2d(bb.0.x..bb.1.x, bb.0.y..bb.1.y)?;
 
     chart.configure_mesh().draw()?;
 
-    chart
-        .draw_series(LineSeries::new(
-            (-50..=50).map(|x| x as f32 / 50.0).map(|x| {
-                let result = bezier.compute(x);
-                (result.x, result.y)
-            }),
-            &RED,
-        ))?
-        .label(control_str)
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+    chart.draw_series(LineSeries::new(
+        (-50..=50).map(|x| x as f32 / 50.0).map(|x| {
+            let result = bezier.compute(x);
+            (result.x, result.y)
+        }),
+        &RED,
+    ))?;
 
     chart
         .configure_series_labels()
