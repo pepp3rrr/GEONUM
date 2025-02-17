b0de0050c8b05d8a00244ab861743382a978f64a
GaspardCulis <gaspard@gasdev.fr>
2025-02-17 10:57:25 +0100

    feat(tp4): Create `subdivision` mod with `SubdivisionCurve` impl

--- tp4/src/main.rs
+++ tp4/src/main.rs
@@ -1,3 +1,12 @@
+use geonum_common::FromCSV;
+use subdivision::SubdivisionCurve;
+
+mod subdivision;
+
 fn main() {
-    println!("Hello, world!");
+    let subdivision = SubdivisionCurve::from_csv("tp4/data/simple.data");
+
+    let points = subdivision.compute_chaikin(4);
+
+    println!("{:?}", points);
 }
---
+++ tp4/src/subdivision.rs
@@ -0,0 +1,49 @@
+use geonum_common::{FromCSV, Point};
+
+pub struct SubdivisionCurve {
+    control: Vec<Point>,
+}
+
+impl SubdivisionCurve {
+    pub fn compute_chaikin(self, steps: u32) -> Vec<Point> {
+        if steps == 0 {
+            return self.control;
+        }
+
+        let n = self.control.len();
+        let mut new = Vec::<Point>::with_capacity(2 * n);
+
+        self.control.windows(2).for_each(|k| {
+            let [xi, xi_1]: [Point; 2] = k.try_into().expect("Should be size 2");
+
+            let x2i = (3. / 4. * xi + 1. / 4. * xi_1).into_point();
+            let x2i_1 = (1. / 4. * xi + 3. / 4. * xi_1).into_point();
+
+            new.push(x2i);
+            new.push(x2i_1);
+        });
+
+        SubdivisionCurve { control: new }.compute_chaikin(steps - 1)
+    }
+}
+
+impl FromCSV for SubdivisionCurve {
+    fn read(mut reader: geonum_common::CSVReader) -> Self {
+        let control = reader
+            .records()
+            .map(|result| {
+                let record = result.expect("Failed to read record");
+
+                let x_str = record.get(0).unwrap();
+                let y_str = record.get(1).unwrap();
+
+                let x = x_str.parse().unwrap();
+                let y = y_str.parse().unwrap();
+
+                Point::new(x, y)
+            })
+            .collect();
+
+        Self { control }
+    }
+}
