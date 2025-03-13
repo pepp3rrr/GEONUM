80975ab2234fbaecba1147925775a9d31e4eb7ea
GaspardCulis <gaspard@gasdev.fr>
2025-03-13 20:56:25 +0100

    feat(tp6): Add simple 3d renderer with cam controller

--- tp6/src/main.rs
+++ tp6/src/main.rs
@@ -1,3 +1,60 @@
+use blue_engine::{
+    header::{Engine, WindowDescriptor},
+    primitive_shapes::triangle,
+    KeyCode, ObjectSettings, Vector3,
+};
+
+const MOVE_SPEED: f32 = 10f32;
+
 fn main() {
-    println!("Hello, world!");
+    let mut engine =
+        Engine::new_config(WindowDescriptor::default()).expect("Couldn't init the Engine");
+
+    triangle(
+        "Triangle",
+        ObjectSettings::default(),
+        &mut engine.renderer,
+        &mut engine.objects,
+    );
+
+    let mut time = std::time::Instant::now();
+
+    engine
+        .update_loop(
+            move |_renderer, _window, _objects, events, camera, _plugins| {
+                let main_camera = camera.cameras.get("main").expect("No main camera");
+                let camera_position = main_camera.position.clone();
+                let delta = time.elapsed().as_secs_f32();
+
+                let direction_vector = Vector3 {
+                    x: if events.key_held(KeyCode::KeyA) {
+                        -1.0
+                    } else if events.key_held(KeyCode::KeyD) {
+                        1.0
+                    } else {
+                        0.0
+                    },
+                    y: if events.key_held(KeyCode::ShiftLeft) {
+                        -1.0
+                    } else if events.key_held(KeyCode::Space) {
+                        1.0
+                    } else {
+                        0.0
+                    },
+                    z: if events.key_held(KeyCode::KeyW) {
+                        -1.0
+                    } else if events.key_held(KeyCode::KeyS) {
+                        1.0
+                    } else {
+                        0.0
+                    },
+                };
+
+                camera.set_position(camera_position + direction_vector * MOVE_SPEED * delta);
+                camera.set_target(camera_position - Vector3::UNIT_Z);
+
+                time = std::time::Instant::now();
+            },
+        )
+        .expect("Error during update loop");
 }
