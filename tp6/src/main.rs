cb2b7202d619910089384e844ea5a739c4a7ec60
GaspardCulis <gaspard@gasdev.fr>
2025-03-16 15:12:16 +0100

    feat(tp6): Create `render` module with new `IntoMesh` trait

--- tp6/src/main.rs
+++ tp6/src/main.rs
@@ -1,28 +1,26 @@
 use bezier_surface::PiecewiseBezierSurface;
 use blue_engine::{
     header::{Engine, WindowDescriptor},
-    wgpu, KeyCode, ObjectSettings, RotateAmount, RotateAxis, ShaderSettings, Vector2, Vector3,
-    Vertex,
+    wgpu, KeyCode, ObjectSettings, RotateAmount, RotateAxis, ShaderSettings, Vector3,
 };
 use geonum_common::FromCSV as _;
+use render::IntoMesh;
 
 mod bezier_surface;
+mod render;
 
 const MOVE_SPEED: f32 = 10f32;
 const SAMPLES_PER_AXIS: u32 = 12;
 
 fn main() {
-    let surface = PiecewiseBezierSurface::from_csv("tp6/data/teapot.bpt");
-
     let mut engine =
         Engine::new_config(WindowDescriptor::default()).expect("Couldn't init the Engine");
 
-    let mut vertices = Vec::new();
-    let mut indices = Vec::new();
+    let surface = PiecewiseBezierSurface::from_csv("tp6/data/teapot.bpt");
 
-    let mut i = 0;
-    for patch in surface.patches {
-        let mut grid = Vec::new();
+    let mut figure_patch_meshes = Vec::new();
+    for (n, patch) in surface.patches.into_iter().enumerate() {
+        let mut samples = Vec::new();
         for i in 0..(SAMPLES_PER_AXIS + 1) {
             let mut row = Vec::new();
             for j in 0..(SAMPLES_PER_AXIS + 1) {
@@ -33,78 +31,36 @@ fn main() {
                 let sample = patch.evalutate(u, v);
                 row.push(sample);
             }
-            grid.push(row);
+            samples.push(row);
         }
-        // grid = patch.control;
 
-        for x in 0..grid.len() {
-            let row = grid[x].clone();
-            for y in 0..row.len() {
-                let b_x_y = row[y];
-                if x + 1 < grid.len() && y + 1 < row.len() {
-                    let b_xp1_y = grid[x + 1][y];
-                    let b_x_yp1 = row[y + 1];
-                    let b_xp1_yp1 = grid[x + 1][y + 1];
+        let (vertices, indices) = samples.into_mesh();
+        let id = format!("figure-{}", n);
 
-                    vertices.push(Vertex {
-                        position: Vector3::new(b_x_y.x(), b_x_y.z(), b_x_y.y()),
-                        uv: Vector2::ZERO,
-                        normal: Vector3::ZERO,
-                    });
-                    vertices.push(Vertex {
-                        position: Vector3::new(b_x_yp1.x(), b_x_yp1.z(), b_x_yp1.y()),
-                        uv: Vector2::ZERO,
-                        normal: Vector3::ZERO,
-                    });
-                    vertices.push(Vertex {
-                        position: Vector3::new(b_xp1_y.x(), b_xp1_y.z(), b_xp1_y.y()),
-                        uv: Vector2::ZERO,
-                        normal: Vector3::ZERO,
-                    });
-                    vertices.push(Vertex {
-                        position: Vector3::new(b_xp1_yp1.x(), b_xp1_yp1.z(), b_xp1_yp1.y()),
-                        uv: Vector2::ZERO,
-                        normal: Vector3::ZERO,
-                    });
-                    indices.push(i);
-                    indices.push(i + 1);
-                    indices.push(i + 2);
-                    indices.push(i + 2);
-                    indices.push(i + 1);
-                    indices.push(i + 3);
-                    i += 4;
-                }
-            }
-        }
-    }
-
-    engine.objects.new_object(
-        "Figure",
-        vertices,
-        indices,
-        ObjectSettings {
-            shader_settings: ShaderSettings {
-                polygon_mode: wgpu::PolygonMode::Line,
-                cull_mode: None,
+        engine.objects.new_object(
+            id.clone(),
+            vertices,
+            indices,
+            ObjectSettings {
+                shader_settings: ShaderSettings {
+                    polygon_mode: wgpu::PolygonMode::Line,
+                    cull_mode: None,
+                    ..Default::default()
+                },
                 ..Default::default()
             },
-            ..Default::default()
-        },
-        &mut engine.renderer,
-    );
+            &mut engine.renderer,
+        );
+
+        figure_patch_meshes.push(id);
+    }
 
     let mut time = std::time::Instant::now();
 
     engine
         .update_loop(
-            move |_renderer, _window, objects, events, camera, _plugins| {
+            move |_renderer, _window, _objects, events, camera, _plugins| {
                 let delta = time.elapsed().as_secs_f32();
-
-                let figure = objects.get_mut("Figure").unwrap();
-
-                // figure.rotate(RotateAmount::Radians(delta / 3.0), RotateAxis::X);
-                figure.rotate(RotateAmount::Radians(delta), RotateAxis::Y);
-                // figure.rotate(RotateAmount::Radians(delta / 2.0), RotateAxis::Z);
 
                 let main_camera = camera.cameras.get("main").expect("No main camera");
                 let camera_position = main_camera.position.clone();
---
+++ tp6/src/render.rs
@@ -0,0 +1,54 @@
+use blue_engine::{UnsignedIntType, Vector2, Vector3, Vertex};
+use geonum_common::Point;
+
+pub trait IntoMesh {
+    fn into_mesh(&self) -> (Vec<Vertex>, Vec<UnsignedIntType>);
+}
+
+impl IntoMesh for Vec<Vec<Point<3>>> {
+    fn into_mesh(&self) -> (Vec<Vertex>, Vec<UnsignedIntType>) {
+        let mut i = 0;
+        let mut vertices = Vec::new();
+        let mut indices = Vec::new();
+
+        for (x, row) in self.iter().enumerate() {
+            for (y, b_x_y) in row.iter().enumerate() {
+                if x + 1 < self.len() && y + 1 < row.len() {
+                    let b_xp1_y = self[x + 1][y];
+                    let b_x_yp1 = row[y + 1];
+                    let b_xp1_yp1 = self[x + 1][y + 1];
+
+                    vertices.push(Vertex {
+                        position: Vector3::new(b_x_y.x(), b_x_y.z(), b_x_y.y()),
+                        uv: Vector2::ZERO,
+                        normal: Vector3::ZERO,
+                    });
+                    vertices.push(Vertex {
+                        position: Vector3::new(b_x_yp1.x(), b_x_yp1.z(), b_x_yp1.y()),
+                        uv: Vector2::ZERO,
+                        normal: Vector3::ZERO,
+                    });
+                    vertices.push(Vertex {
+                        position: Vector3::new(b_xp1_y.x(), b_xp1_y.z(), b_xp1_y.y()),
+                        uv: Vector2::ZERO,
+                        normal: Vector3::ZERO,
+                    });
+                    vertices.push(Vertex {
+                        position: Vector3::new(b_xp1_yp1.x(), b_xp1_yp1.z(), b_xp1_yp1.y()),
+                        uv: Vector2::ZERO,
+                        normal: Vector3::ZERO,
+                    });
+                    indices.push(i);
+                    indices.push(i + 1);
+                    indices.push(i + 2);
+                    indices.push(i + 2);
+                    indices.push(i + 1);
+                    indices.push(i + 3);
+                    i += 4;
+                }
+            }
+        }
+
+        (vertices, indices)
+    }
+}
