60433abe0f0fcc57f0efa24acd79611f3bf77a4d
GaspardCulis <gaspard@gasdev.fr>
2025-03-24 10:55:55 +0100

    feat(tp3): Got kind of an implem Only working on simple example

--- tp3/src/bspline.rs
+++ tp3/src/bspline.rs
@@ -2,8 +2,51 @@ use geonum_common::{FromCSV, Point};
 
 #[derive(Debug)]
 pub struct BSpline {
+    pub degree: usize,
     pub control: Vec<Point>,
     pub knots: Vec<f32>,
+}
+
+impl BSpline {
+    pub fn sample(&self, density: usize) -> Vec<Point> {
+        let mut out = Vec::new();
+
+        for j in self.degree..(self.knots.len() - self.degree) {
+            // Skip identical
+            if self.knots[j] == self.knots[j + 1] {
+                continue;
+            }
+
+            let mut new = (0..density)
+                .map(|n| {
+                    let k = self.knots[j] + (n as f32 / density as f32) * self.knots[j + 1];
+                    self.compute_worker(self.degree, j, k)
+                })
+                .collect::<Vec<_>>();
+
+            out.append(&mut new);
+        }
+
+        out
+    }
+
+    fn compute_worker(&self, r: usize, j: usize, t: f32) -> Point {
+        if r == 0 {
+            return self.control[j];
+        }
+
+        let d = self.knots[j + self.degree - (r - 1)] - self.knots[j];
+        let w = if d == 0.0 {
+            0.0
+        } else {
+            (t - self.knots[j]) / d
+        };
+
+        let d_jm1 = self.compute_worker(r - 1, j - 1, t);
+        let d_j = self.compute_worker(r - 1, j, t);
+
+        ((1.0 - w) * d_jm1 + w * d_j).into_point()
+    }
 }
 
 impl FromCSV for BSpline {
@@ -43,6 +86,14 @@ impl FromCSV for BSpline {
             knots.push(knot);
         }
 
-        Self { control, knots }
+        let n = control.len() - 1;
+        let m = knots.len() - 1;
+        let degree = m - n - 1;
+
+        Self {
+            control,
+            knots,
+            degree,
+        }
     }
 }
--- tp3/src/main.rs
+++ tp3/src/main.rs
@@ -1,10 +1,96 @@
-use bspline::BSpline;
-use geonum_common::FromCSV;
+use clap::Parser;
+use geonum_common::{BoundingBox, FromCSV};
+use plotters::{element::DashedPathElement, prelude::*};
 
 mod bspline;
 
-fn main() {
-    let spline = BSpline::from_csv("tp3/data/simple.bspline");
+use bspline::BSpline;
 
-    println!("{:?}", spline);
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The path of the BSpline file to plot
+    #[arg()]
+    bspline_path: String,
+
+    /// The output path of the plotted image
+    #[arg(short, long)]
+    output: String,
+
+    /// Number of datapoints to sample
+    #[arg(short, long, default_value_t = 100)]
+    samples: usize,
+
+    /// Wether to draw intermediate control polygons
+    #[arg(short, long)]
+    draw_control: bool,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    let spline = BSpline::from_csv(&args.bspline_path);
+    let bb = spline.control.bounding_box();
+
+    let root = SVGBackend::new(&args.output, (1080, 720)).into_drawing_area();
+    root.fill(&WHITE)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .caption("Bezier curve", ("sans-serif", 50).into_font())
+        .margin(5)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_cartesian_2d(bb.0.x()..bb.1.x(), bb.0.y()..bb.1.y())?;
+
+    chart.configure_mesh().draw()?;
+
+    chart
+        .draw_series(LineSeries::new(
+            spline
+                .sample(args.samples)
+                .into_iter()
+                .map(|p| (p.x(), p.y())),
+            &RED,
+        ))?
+        .label(args.bspline_path);
+
+    if args.draw_control {
+        root.draw(&DashedPathElement::new(
+            spline
+                .control
+                .iter()
+                .map(|p| chart.backend_coord(&(p.x(), p.y()))),
+            4,
+            2,
+            &BLACK,
+        ))?;
+
+        spline
+            .control
+            .iter()
+            .enumerate()
+            .for_each(|(index, point)| {
+                root.draw(&Circle::new(
+                    chart.backend_coord(&(point.x(), point.y())),
+                    if index == 0 || index == (spline.control.len() - 1) {
+                        5
+                    } else {
+                        3
+                    },
+                    Into::<ShapeStyle>::into(&BLACK).filled(),
+                ))
+                .unwrap();
+            });
+    }
+
+    chart
+        .configure_series_labels()
+        .legend_area_size(0)
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
+
+    root.present()?;
+
+    Ok(())
 }
