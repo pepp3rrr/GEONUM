b3434f450213eca61c1092ac169fd986327c9083
GaspardCulis <gaspard@gasdev.fr>
2025-03-30 13:08:14 +0200

    fix(tp8): Fixed invalid `closed_x` (and y) usage

--- tp8/src/subdivision_surface.rs
+++ tp8/src/subdivision_surface.rs
@@ -19,31 +19,27 @@ impl SubdivisionSurface {
 
         let h = self.control.len();
 
-        // Duplication
         let mut new = Vec::with_capacity(h * 2);
-        for line in self.control.iter() {
-            let mut new_row = Vec::with_capacity(2 * line.len());
-            for point in line {
-                // Cloning point data is kinda useless
-                new_row.push(point.clone());
-                new_row.push(point.clone());
-            }
-            new.push(new_row.clone());
-            new.push(new_row);
-        }
 
-        for (i, line) in self.control.iter().enumerate() {
+        for (j, line) in self.control.iter().enumerate() {
+            if !self.closed_x && j + 1 == h {
+                continue;
+            }
+
             let w = line.len();
 
-            for j in 0..w {
-                if self.closed_y && i + 1 == h || self.closed_x && j + 1 == w {
+            let mut new_line_j = Vec::with_capacity(w * 2);
+            let mut new_line_jp1 = Vec::with_capacity(w * 2);
+
+            for i in 0..w {
+                if !self.closed_y && i + 1 == w {
                     continue;
                 }
 
-                let v_i_j = self.control[i][j];
-                let v_ip1_j = self.control[(i + 1) % h][j];
-                let v_i_jp1 = self.control[i][(j + 1) % w];
-                let v_ip1_jp1 = self.control[(i + 1) % h][(j + 1) % w];
+                let v_i_j = self.control[j][i];
+                let v_ip1_j = self.control[j][(i + 1) % w];
+                let v_i_jp1 = self.control[(j + 1) % h][i];
+                let v_ip1_jp1 = self.control[(j + 1) % h][(i + 1) % w];
 
                 let v_2i_2j =
                     1.0 / 16.0 * (9.0 * v_i_j + 3.0 * v_ip1_j + 3.0 * v_i_jp1 + 1.0 * v_ip1_jp1);
@@ -54,11 +50,13 @@ impl SubdivisionSurface {
                 let v_2ip1_2jp1 =
                     1.0 / 16.0 * (1.0 * v_i_j + 3.0 * v_ip1_j + 3.0 * v_i_jp1 + 9.0 * v_ip1_jp1);
 
-                new[2 * i][2 * j] = v_2i_2j.into_point();
-                new[2 * i + 1][2 * j] = v_2ip1_2j.into_point();
-                new[2 * i][2 * j + 1] = v_2i_2jp1.into_point();
-                new[2 * i + 1][2 * j + 1] = v_2ip1_2jp1.into_point();
+                new_line_j.push(v_2i_2j.into_point());
+                new_line_j.push(v_2ip1_2j.into_point());
+                new_line_jp1.push(v_2i_2jp1.into_point());
+                new_line_jp1.push(v_2ip1_2jp1.into_point());
             }
+            new.push(new_line_j);
+            new.push(new_line_jp1);
         }
 
         SubdivisionSurface {
