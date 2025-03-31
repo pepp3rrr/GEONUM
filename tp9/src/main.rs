256b9585c0982e755422521be7838a19e0bdcc5b
GaspardCulis <gaspard@gasdev.fr>
2025-03-31 10:54:56 +0200

    feat(tp9): Impl `FromCSV` for `TriangleMesh`

--- tp9/src/main.rs
+++ tp9/src/main.rs
@@ -1,5 +1,10 @@
+use geonum_common::FromCSV as _;
+use triangle_mesh::TriangleMesh;
+
 mod triangle_mesh;
 
 fn main() {
-    println!("Hello, world!");
+    let mesh = TriangleMesh::from_csv("tp9/data/testsurf.off");
+
+    println!("{:?}", mesh);
 }
--- tp9/src/triangle_mesh.rs
+++ tp9/src/triangle_mesh.rs
@@ -1,6 +1,43 @@
-use geonum_common::Point;
+use geonum_common::{FromCSV, Point};
 
+#[derive(Debug)]
 pub struct TriangleMesh {
-    vertex: Vec<Point<3>>,
-    indices: Vec<(i32, i32, i32)>,
+    vertices: Vec<Point<3>>,
+    indices: Vec<(usize, usize, usize)>,
+}
+
+impl FromCSV for TriangleMesh {
+    fn read(mut reader: geonum_common::CSVReader) -> Self {
+        let mut iter = reader.records();
+        let meta = iter.next().unwrap().unwrap();
+
+        let num_vertices = *&meta[0].parse::<usize>().unwrap();
+        let num_indices = *&meta[1].parse::<usize>().unwrap();
+
+        let mut vertices = Vec::with_capacity(num_vertices);
+
+        for _ in 0..num_vertices {
+            let line = iter.next().unwrap().unwrap();
+
+            let x: f32 = *&line[0].parse().unwrap();
+            let y: f32 = *&line[1].parse().unwrap();
+            let z: f32 = *&line[2].parse().unwrap();
+
+            vertices.push(Point::<3>::new(x, y, z));
+        }
+
+        let mut indices = Vec::with_capacity(num_indices);
+
+        for _ in 0..num_indices {
+            let line = iter.next().unwrap().unwrap();
+
+            let a: usize = *&line[1].parse().unwrap();
+            let b: usize = *&line[2].parse().unwrap();
+            let c: usize = *&line[3].parse().unwrap();
+
+            indices.push((a, b, c));
+        }
+
+        Self { vertices, indices }
+    }
 }
