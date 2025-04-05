378d59288a1986f3c3a2066e6130206a9b07ae51
GaspardCulis <gaspard@gasdev.fr>
2025-04-05 08:33:43 +0200

    feat(tp9): Implement complete loop subdivision scheme

--- tp9/src/triangle_mesh.rs
+++ tp9/src/triangle_mesh.rs
@@ -6,14 +6,112 @@ use std::{
 use blue_engine::{Vector2, Vector3, Vertex};
 use geonum_common::{FromCSV, IntoMesh, Mesh, Point};
 
+type Edge = (usize, usize);
+type Face = (usize, usize, usize);
+
 #[derive(Debug)]
 pub struct TriangleMesh {
     vertices: Vec<Point<3>>,
-    indices: Vec<(usize, usize, usize)>,
+    indices: Vec<Face>,
 }
 
 impl TriangleMesh {
     pub fn subdivide(&self) -> Self {
+        let m = self.indices.len();
+        let mut n = self.vertices.len();
+        let mut new_vertices = Vec::new();
+        let mut new_indices = Vec::with_capacity(m * 4);
+
+        let mut visited_edges = HashMap::<Edge, usize>::new();
+        for face in &self.indices {
+            let a = (face.0, face.1);
+            let b = (face.1, face.2);
+            let c = (face.2, face.0);
+
+            let new_points = [a, b, c].map(|edge| {
+                if visited_edges.contains_key(&edge) {
+                    return visited_edges.get(&edge).cloned().unwrap();
+                }
+
+                let edge_points = (self.vertices[edge.0], self.vertices[edge.1]);
+                let adjacent_faces = self.get_adjacent_faces(&edge);
+                assert_eq!(adjacent_faces.len(), 2);
+
+                // Points of the adjacent faces not part of the current edge
+                let extreme_indices = adjacent_faces
+                    .into_iter()
+                    .map(|face| {
+                        [face.0, face.1, face.2]
+                            .into_iter()
+                            .find(|i| i != &edge.0 && i != &edge.1)
+                            .unwrap()
+                    })
+                    .collect::<Vec<_>>();
+                let extreme_points = (
+                    self.vertices[extreme_indices[0]],
+                    self.vertices[extreme_indices[1]],
+                );
+
+                let midpoint = (3.0 / 8.0 * (edge_points.0 + edge_points.1)
+                    + 1.0 / 8.0 * (extreme_points.0 + extreme_points.1))
+                    .into_point();
+                let midpoint_index = n;
+
+                new_vertices.push(midpoint);
+                visited_edges.insert(edge, n);
+                n += 1;
+
+                midpoint_index
+            });
+
+            new_indices.push((new_points[0], new_points[2], face.0));
+            new_indices.push((new_points[1], new_points[0], face.1));
+            new_indices.push((new_points[2], new_points[1], face.2));
+            new_indices.push((new_points[0], new_points[1], new_points[2]));
+        }
+
+        let edges = visited_edges.into_keys().collect::<Vec<_>>();
+
+        let final_vertices = self
+            .vertices
+            .clone()
+            .into_iter()
+            .enumerate()
+            .map(|(index, vertex)| {
+                let neighbours = edges
+                    .iter()
+                    .filter_map(|edge| {
+                        if edge.0 == index {
+                            Some(edge.1)
+                        } else if edge.1 == index {
+                            Some(edge.0)
+                        } else {
+                            None
+                        }
+                    })
+                    .filter(|x| x != &index)
+                    .collect::<Vec<_>>();
+
+                let n = neighbours.len() as f32;
+                let beta = 1. / n * (5. / 8. - (3. / 8. + 1. / 4. * (2. * PI / n).cos()).powi(2));
+
+                neighbours
+                    .into_iter()
+                    .fold(vertex.clone() * (1.0 - n * beta), |acc, x| {
+                        acc + self.vertices[x] * beta
+                    })
+                    .into_point()
+            })
+            .chain(new_vertices)
+            .collect();
+
+        Self {
+            vertices: final_vertices,
+            indices: new_indices,
+        }
+    }
+
+    pub fn subdivide_old(&self) -> Self {
         let edges = self.get_edges();
         let mut new_vertices = Vec::with_capacity(edges.len());
 
@@ -80,7 +178,7 @@ impl TriangleMesh {
         }
     }
 
-    fn get_edges(&self) -> HashSet<(usize, usize)> {
+    fn get_edges(&self) -> HashSet<Edge> {
         let mut edge_indices = HashSet::new();
 
         for face in &self.indices {
@@ -96,7 +194,7 @@ impl TriangleMesh {
         edge_indices
     }
 
-    fn get_adjacent_faces(&self, edge: &(usize, usize)) -> Vec<(usize, usize, usize)> {
+    fn get_adjacent_faces(&self, edge: &Edge) -> Vec<Face> {
         let mut adjacent = Vec::new();
 
         for face in &self.indices {
@@ -110,7 +208,7 @@ impl TriangleMesh {
         adjacent
     }
 
-    fn get_neighbour_faces(&self, edge: &(usize, usize)) -> Vec<(usize, usize, usize)> {
+    fn get_neighbour_faces(&self, edge: &Edge) -> Vec<Face> {
         let mut neighbours = Vec::new();
 
         for face in &self.indices {
@@ -125,7 +223,7 @@ impl TriangleMesh {
         neighbours
     }
 
-    fn get_neighbour_vertices(&self, edge: &(usize, usize)) -> Vec<Point<3>> {
+    fn get_neighbour_vertices(&self, edge: &Edge) -> Vec<Point<3>> {
         let mut neighbour_vertices = HashMap::new();
 
         for face in self.get_neighbour_faces(edge) {
